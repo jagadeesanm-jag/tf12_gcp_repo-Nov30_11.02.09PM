c46ccdcd5ca68b1820bab4c5f852cfa4cbb570f2
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-11-30 23:02:48 +0530

    Modified files:[main.tf]

--- gcp_compute_firewall_allow_ssh/main.tf
+++ gcp_compute_firewall_allow_ssh/main.tf
@@ -53,3 +53,5 @@ resource "google_compute_firewall" "default_firewall" {
 output "ip" {
  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+
+# Modified [main.tf] on [2020-11-30 23:02:48.639077]
