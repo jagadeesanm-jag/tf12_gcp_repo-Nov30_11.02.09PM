5e6468b53ac1885ab36cb26f6fbc38c5fb7ee1b0
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-11-30 23:02:42 +0530

    Modified files:[gcp_all_issues.tf]

--- gcp_all_issues/gcp_all_issues.tf
+++ gcp_all_issues/gcp_all_issues.tf
@@ -350,3 +350,4 @@ resource "google_project_iam_member" "sql_client3" {
     role = "roles/cloudkms.admin"
     member = "user:abc@gmail.com"
 }
+# Modified [gcp_all_issues.tf] on [2020-11-30 23:02:42.382604]
