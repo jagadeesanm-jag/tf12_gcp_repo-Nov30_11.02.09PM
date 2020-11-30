6feb94b586f511bd3c8f9d7c2b14213d9d223fc4
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-11-30 23:02:31 +0530

    Modified files:[main.tf]

--- gcp_compute_output/main.tf
+++ gcp_compute_output/main.tf
@@ -37,3 +37,5 @@ resource "google_compute_instance" "default" {
 output "ip" {
  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+
+# Modified [main.tf] on [2020-11-30 23:02:31.165893]
